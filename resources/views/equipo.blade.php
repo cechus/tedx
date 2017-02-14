4018ccb349d9f3767a826b9ba23c6a758b91313e
= <=>
2017-02-14 16:33:04 -0400

    Cambios en: EQUIPO, LOGOted

--- resources/views/equipo.blade.php
+++ resources/views/equipo.blade.php
@@ -5,15 +5,183 @@
         <div class="section scrollspy" id="services">
             <div class="container">
                 <div class="row">
-                    <h2>Aliados</h2>
+                    <h2>Equipo</h2>
                     <div class="col s12 m10 offset-m1 l8 offset-l2">
                         <p class="flow-text center-align">
-                            We're a small local company focused on crafting special and precise products that really help in
-                            your everyday life. Feel free to check out our services and explore the gallery of our works.
+                            Nosotros somos el equipo organizador de TEDxUMSA
                         </p>
                     </div>
+                        <!-- equipo tedX , recorrer con fetch-->
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s12 m4">
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                  <img class="activator" src="images/team/desconocido.png">
+                                </div>
+                                <div class="card-content">
+                                  <span class="card-title activator grey-text text-darken-4">
+                                  Fulanito Perez<i class="material-icons right">more_vert</i></span>
+                                  <p>Aquí va mi cargo.</p>
+                                </div>
+                                <div class="card-reveal">
+                                  <span class="card-title grey-text text-darken-4">Fulanito Perez
+                                  <i class="material-icons right">close</i></span>
+                                  <h6>Aquí va mi cargo</h6>
+                                  <p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>
+                                </div>
+                            </div>
+                        </div>
                 </div>
-                <!-- Row with service icons -->
+                <br><br><br><br><br><br>
+                <!--
+                <div class="row">
+                    
+                </div>
+                -->
+                <!-- Row with service icons 
                 <div class="row services">
                     <div class="col s12 l4">
                         <i class="fa fa-5x fa-paint-brush animated-color"></i>
@@ -36,9 +204,9 @@
                         <p>You are welcome to customize the elements to fit your needs. The proccess is simple and pleasant thanks for a Material color palette</p>
                         <a class="btn waves-effect waves-dark white black-text">Try it</a>
                     </div>
-                    
                 </div>
-                <!--plat-->
+                -->
+                <!--plat
                 <div class="row services">
                     <div class="col s12 l3">
                         <i class="fa fa-5x fa-paint-brush animated-color"></i>
@@ -97,8 +265,8 @@
                         <a class="btn waves-effect waves-dark white black-text">Try it</a>
                     </div>
                 </div>
-
-                <!--amigos-->
+                -->
+                <!--amigos
                 <div class="row services">
                     <div class="col s12 l1">
                         <i class="fa fa-5x fa-paint-brush animated-color"></i>
@@ -201,6 +369,8 @@
                         <div class="divider"></div>
                     </div>
                 </div>
+                -->
+                
             </div>
         </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="libs/materialize/css/materialize.min.css">
     <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/wata.css">
-    <!-- Scripts -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Scripts-->
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
@@ -26,10 +27,11 @@
         <div class="navbar-fixed" id="nav">
             <nav>
                 <div class="nav-wrapper">
-
                     <!-- Site name -->
-                    <a href="/" class="brand-logo">TEDxUMSA</a>
-
+                    <a href="/">
+                        <img src="/images/logo/TED.svg" style="width: 15%; height: auto">
+                    </a>
+                    
                     <!-- Mobile menu button -->
                     <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
 
@@ -64,8 +66,12 @@
 
         <!-- Footer section -->
         <footer class="page-footer">
+                    
             <div class="footer-copyright">
                 <div class="container">
+                    <a href="/">
+                        <img src="/images/logo/TEDxUMSA.svg" style="width: 10%; height: auto;">
+                    </a>
                             © 2017 TEDX | <a href="ajax/pages/privacy-policy.html" class="ajax-link">Privacy policy</a>
                             <!-- Social icons -->
                     <span class="social right">
