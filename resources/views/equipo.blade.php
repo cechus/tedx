01b63e55616a753abcbe1bb6bce41070de30998d
cechus <cechus_orh@hotmail.com>
2017-03-05 18:32:24 -0300

    Fix bugs and added meta tags for social networks

--- resources/views/equipo.blade.php
+++ resources/views/equipo.blade.php
@@ -1,155 +1,161 @@
 @extends('layouts.app')
-<style>
-
-</style>
 @section('content')
+<style>
+	.col{
+		display: inline-block;
+	}
+</style>
  <!-- Equipo section -->
-        <div class="section scrollspy">
-            <div class="container">
-                <div class="row">
-                    <h2>Equipo</h2>
-                        <p class="flow-text center-align">
-                            Nosotros somos el equipo organizador de TEDxUMSA
-                        </p>
-                        <!-- equipo tedX , recorrer con fetch-->
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light" >
-                                <!--<div class="card-image waves-effect waves-block waves-light" onMouseOver="cambiar();" onMouseOut="volver();">-->
-                                  <img class="img-responsive activator" id="carlos" src="images/team/carlos.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Carlos Zabaleta<i class="material-icons right">more_vert</i></span>
-                                  <p>Licenciatario/Organizador</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Carlos Zabaleta Copa</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Licenciatario/Organizador</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/ana.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Ana Quispe<i class="material-icons right">more_vert</i></span>
-                                  <p>Directora de protocolo y Produccion ejecutiva</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Ana Quispe Sirpa</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Directora de protocolo y Produccion ejecutiva</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/herman.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Hermann Lino<i class="material-icons right">more_vert</i></span>
-                                  <p>Director de Ventas</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Hermann Lino</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Director de Ventas</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/veronica.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Veronica Clavijo<i class="material-icons right">more_vert</i></span>
-                                  <p>Directora de relaciones externas y Curaduría</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Veronica Clavijo Altamirano</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Directora de relaciones externas y Curaduría</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/andres.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Andrés Mamani<i class="material-icons right">more_vert</i></span>
-                                  <p>Director de Media</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Andrés Mamani</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Director de Media</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/kevin.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Kevin Vásquez<i class="material-icons right">more_vert</i></span>
-                                  <p>Director Creativo de Diseño</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Kevin Vásquez</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Director Creativo de Diseño</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col s12 m6 l4">
-                            <div class="card">
-                                <div class="card-image waves-effect waves-block waves-light">
-                                  <img class="img-responsive activator" src="images/team/kari.jpg">
-                                </div>
-                                <div class="card-content">
-                                  <span class="card-title activator grey-text text-darken-4">
-                                  Karina Llanos<i class="material-icons right">more_vert</i></span>
-                                  <p>Directora de Marketing y Relaciones Publicas</p>
-                                </div>
-                                <div class="card-reveal">
-                                  <span class="card-title grey-text text-darken-4"><b>Karina Alejandra Llanos</b>
-                                  <i class="material-icons right">close</i></span>
-                                  <h6>Directora de Marketing y Relaciones Publicas</h6>
-                                  <!--<p>Aqui va informacion acerca de mi persona. Soy parte del equipo organizador de TEDxUMSA</p>-->
-                                </div>
-                            </div>
-                        </div>
-                </div>
-                <br><br><br><br><br><br>
-            </div>
-        </div>
-        <script type="text/javascript">
-         function cambiar () {
-          document.getElementById('carlos').src = "images/team/andres.jpg";
-         }
-         function volver () {
-          document.getElementById('carlos').src = "images/team/carlos.jpg";
-         }
-        </script>
+		<div class="section scrollspy">
+			<div class="container">
+					<h2>Equipo</h2>
+						<p class="flow-text center-align">
+							Nosotros somos el equipo organizador de TEDxUMSA
+						</p>
+				<div class="row" id="equipo" >
+					<!-- equipo tedX , recorrer con fetch-->
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light" >
+							  <img class="img-responsive activator" id="carlos" src="images/team/carlos-1.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Carlos Zabaleta<i class="material-icons right">more_vert</i></span>
+							  <p>Licenciatario/Organizador</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Carlos Zabaleta Copa</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Licenciatario/Organizador</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/ana-1.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Ana Quispe<i class="material-icons right">more_vert</i></span>
+							  <p>Directora de protocolo y Produccion ejecutiva</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Ana Quispe Sirpa</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Directora de protocolo y Produccion ejecutiva</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/herman-1.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Herman Lino<i class="material-icons right">more_vert</i></span>
+							  <p>Director de Ventas</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Hermann Lino</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Director de Ventas</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/veronica.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Veronica Clavijo<i class="material-icons right">more_vert</i></span>
+							  <p>Directora de relaciones externas y Curaduría</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Veronica Clavijo Altamirano</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Directora de relaciones externas y Curaduría</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/andres.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Andrés Mamani<i class="material-icons right">more_vert</i></span>
+							  <p>Director de Media</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Andrés Mamani</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Director de Media</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/kevin.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Kevin Vásquez<i class="material-icons right">more_vert</i></span>
+							  <p>Director Creativo de Diseño</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Kevin Vásquez</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Director Creativo de Diseño</h6>
+							</div>
+						</div>
+					</div>
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="img-responsive activator" src="images/team/kari.jpg" />
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text text-darken-4">
+							  Karina Llanos<i class="material-icons right">more_vert</i></span>
+							  <p>Directora de Marketing y Relaciones Publicas</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text text-darken-4"><b>Karina Alejandra Llanos</b>
+							  <i class="material-icons right">close</i></span>
+							  <h6>Directora de Marketing y Relaciones Publicas</h6>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br><br><br><br><br><br>
+			</div>
+		</div>
+		<script type="text/javascript" src="/libs/jquery/jquery-2.1.1.js"></script>
+		  <!--<script type="text/javascript" src="https://unpkg.com/isotope-layout@3.0.2/dist/isotope.pkgd.min.js"></script>-->
+		  <script type="text/javascript" src="/libs/masonry/dist/masonry.min.js"></script>
+			  <script type="text/javascript">
+			  $(document).ready(function() {
+				$('#equipo').masonry({
+					itemSelector: '.col'
+				});
+			});
+		</script>  
+@endsection
+@section('scriptside')
+<script type="text/javascript">
+    $('.button-collapse').sideNav({
+         menuWidth: 300, 
+         edge: 'left',
+         closeOnClick: true,
+         draggable: true 
+       }
+     );
+</script>
 @endsection
--- resources/views/speakers/index.blade.php
+++ resources/views/speakers/index.blade.php
@@ -1,35 +1,155 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+  .col{
+	display: inline-block;
+  }
+</style>
   <div class="section scrollspy" id="services">
-      <div class="container">
-          <div class="row">
-              <h2>Expositores</h2>
-              <div class="col s12 m10 offset-m1 l8 offset-l2">
-              </div>
-                  <!-- Recorriendo los speakers(EXPOSITORES) con foreach-->
-                  @foreach ($speakers as $speak)
-                    <div class="col s12 m6 l4">
-                        <div class="card">
-                            <div class="card-image waves-effect waves-block waves-light">
-                              <img class="activator" src="{{ $speak->imagen}}">
-                            </div>
-                            <div class="card-content">
-                              <span class="card-title activator grey-text text-darken-4">
-                              {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
-                              <p>{{ $speak->charla }}</p>
-                            </div>
-                            <div class="card-reveal">
-                              <span class="card-title grey-text text-darken-4">{{ $speak->nombre }}
-                              <i class="material-icons right">close</i></span>
-                              <h6><b>{{ $speak->charla }}</b></h6>
-                              <p>{{ $speak->descripcion }}</p>
-                            </div>
-                        </div>
-                    </div>
-                  @endforeach
-          </div>
-          <br><br><br><br><br><br>
-      </div>
+	  <div class="container">
+		  <div class="row" id="speakers">
+			  <h2>Expositores</h2>
+				  <!-- Recorriendo los speakers(EXPOSITORES) con foreach-->
+				  @foreach ($speakers as $speak)
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div><div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div><div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div>
+				  @endforeach
+				  @foreach ($speakers as $speak)
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div>
+				  @endforeach
+				  @foreach ($speakers as $speak)
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div>
+				  @endforeach
+				  @foreach ($speakers as $speak)
+					<div class="col s12 m6 l4">
+						<div class="card">
+							<div class="card-image waves-effect waves-block waves-light">
+							  <img class="activator" src="{{ $speak->imagen}}">
+							</div>
+							<div class="card-content">
+							  <span class="card-title activator grey-text ">
+							  {{ $speak->nombre }}<i class="material-icons right">more_vert</i></span>
+							  <p>{{ $speak->charla }}</p>
+							</div>
+							<div class="card-reveal">
+							  <span class="card-title grey-text ">{{ $speak->nombre }}
+							  <i class="material-icons right">close</i></span>
+							  <h6><b>{{ $speak->charla }}</b></h6>
+							  <p>{{ $speak->descripcion }}</p>
+							</div>
+						</div>
+					</div>
+				  @endforeach
+
+		  </div>
+		  <script type="text/javascript" src="/libs/jquery/jquery-2.1.1.js"></script>
+		  <!--<script type="text/javascript" src="https://unpkg.com/isotope-layout@3.0.2/dist/isotope.pkgd.min.js"></script>-->
+		  <script type="text/javascript" src="/libs/masonry/dist/masonry.min.js"></script>
+			  <script type="text/javascript">
+			  $(document).ready(function() {
+				$('#speakers').masonry({
+					itemSelector: '.col'
+				});
+			});
+			  </script>
+		  
+		  <br><br><br><br><br><br>
+	  </div>
   </div>
+@endsection
+@section('scriptside')
+<script type="text/javascript">
+    $('.button-collapse').sideNav({
+         menuWidth: 300, 
+         edge: 'left',
+         closeOnClick: true,
+         draggable: true 
+       }
+     );
+</script>
 @endsection
