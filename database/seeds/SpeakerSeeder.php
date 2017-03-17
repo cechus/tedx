f9b6a71e34ea8b6cc7df1b7830e09531b68ea125
Veuge <nicky.0592@gmail.com>
2017-03-17 15:03:48 -0400

    Alina

--- database/seeds/SpeakerSeeder.php
+++ database/seeds/SpeakerSeeder.php
@@ -30,7 +30,7 @@ class SpeakerSeeder extends Seeder
         DB::table('speakers')->insert([
             'nombre'=>"Alina Santander",
             'charla'=>"",
-            'descripcion'=>"Alina Santander Vinokurova lideró el equipo ganador del auto lunar para una competencia en la NASA y desea motivar a más estudiantes bolivianos.",
+            'descripcion'=>"Joven cientifica de 17 años, ganadora de medalla de oro en astronomia y astrofísica de las Olimpiadas Cientificas Estudiantiles del año 2015 y unica participante boliviana seleccionada para el desafio Human Exploration Rover Challenge organizado por la NASA. Ella junto con su equipo consiguieron el primer lugar en la competencia. Ademas de diseñar parte del vehiculo es la encargada de pilotearlo en el momento de la competencia.",
             'imagen'=>'images/speakers/AlinaSantander.png'
         ]);
     }
