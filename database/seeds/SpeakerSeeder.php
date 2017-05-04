80aec679735e0021cd56960958e336f9fb9f548c
Veuge <nicky.0592@gmail.com>
2017-05-04 11:53:26 -0400

    Optimización de imagenes

--- database/seeds/SpeakerSeeder.php
+++ database/seeds/SpeakerSeeder.php
@@ -27,6 +27,13 @@ class SpeakerSeeder extends Seeder
         ]);
 
         DB::table('speakers')->insert([
+            'nombre'=>"Marco Antonio Quelca",
+            'charla'=>"",
+            'descripcion'=>"Cocinero de profesión, en su etapa de estudiante representó a Bolivia en los concursos de cocina creativa en Lima - Perú, México DF y Montevideo - Uruguay. Ha sido jefe de cocina en Hoteles cinco estrellas de la ciudad de Santa Cruz-Bolivia. Tiene una maestría en cocina contemporánea en el Centro internacional de Estudios Turísticos de Canarias, España y  desde el 2012 hasta la actualidad ha estado viviendo de manera itinerante en Bolivia y España, en este último país estando en restaurantes con estrellas Michelín. Es creador del movimiento Sabor Clandestino, que es compuesto por los proyectos Cascándole y Somos Calle, que buscan reflejar  una gastronomía contemporánea con las raíces e ingredientes tradicionales de la comida popular boliviana, ésto con el objetivo de transmitir nuestras tradiciones y culturas con una visión actual.",
+            'imagen'=>'images/speakers/MarcoQuelca.png'
+        ]);
+
+        DB::table('speakers')->insert([
             'nombre'=>"Leonardo Viscarra",
             'charla'=>"",
             'descripcion'=>"Joven de 14 años investigador y genio boliviano. Experto en robótica. A pesar de haber nacido sin una mano, eso no le ha impedido ser destacado en todos los ambitos de su vida. A corta edad descubrió su pasion por la tecnología. Creador de su propia protesis de mano que es capaz de captar los impulsos de su muñeca para transmitirlos a los dedos, además esta actualmente está elaborando protesis parecidas para otros niños de bajos recursos que por diferentes circunstancias perdieron una de sus extremidades. Fue participante de los Clubes de Ciencia Bolivia como uno de los expositores para compartir con más jovenes su pasión por la robótica.",
@@ -34,27 +41,20 @@ class SpeakerSeeder extends Seeder
         ]);
 
         DB::table('speakers')->insert([
-            'nombre'=>"Marco Antonio Quelca",
+            'nombre'=>"Jazmin Quiroz Calizaya",
             'charla'=>"",
-            'descripcion'=>"Cocinero de profesión, en su etapa de estudiante representó a Bolivia en los concursos de cocina creativa en Lima - Perú, México DF y Montevideo - Uruguay. Ha sido jefe de cocina en Hoteles cinco estrellas de la ciudad de Santa Cruz-Bolivia. Tiene una maestría en cocina contemporánea en el Centro internacional de Estudios Turísticos de Canarias, España y  desde el 2012 hasta la actualidad ha estado viviendo de manera itinerante en Bolivia y España, en este último país estando en restaurantes con estrellas Michelín. Es creador del movimiento Sabor Clandestino, que es compuesto por los proyectos Cascándole y Somos Calle, que buscan reflejar  una gastronomía contemporánea con las raíces e ingredientes tradicionales de la comida popular boliviana, ésto con el objetivo de transmitir nuestras tradiciones y culturas con una visión actual.",
-            'imagen'=>'images/speakers/MarcoQuelca.png'
+            'descripcion'=>"Nació en la ciudad de La Paz y reside en Cochabamba desde sus 3 años. Desde el 2011 descubrió el fascinante mundo de los murciélagos despues de asistir al Congreso de Mastozoologia e inmediatamente decidió trabajar con estos animales, se unió al Programa para la Conservación de los Murciélagos de Bolivia (PCMB). Trabaja como auxiliar del Centro de Biodiversidad y Genética (CBG) en la UMSS. Obtuvo el título de alumnos de IBRO (International Brain Research Organization) en la Escuela Andina de Neuroetologia. Es becaria de Bat Conservation International (BCI) gracias a su proyecto de conservación de las especies de murciélagos en Capinota - Cochabamba y fue premiada con el Women in Conservation Science Award.",
+            'imagen'=>'images/speakers/JazminQuiroz.png'
         ]);
 
-        // DB::table('speakers')->insert([
-        //     'nombre'=>"Jazmin Quiroz",
-        //     'charla'=>"",
-        //     'descripcion'=>"",
-        //     'imagen'=>'images/speakers/JazminQuiroz.png'
-        // ]);
+        DB::table('speakers')->insert([
+            'nombre'=>"Álex Ayala Ugarte",
+            'charla'=>"",
+            'descripcion'=>"Español de nacimiento, boliviano de corazón y tartamudo de vocación. Fue director del dominical del diario La Razón de Bolivia, editor de periodismo narrativo del semanario Pulso y fundador de Pie Izquierdo, primera revista boliviana de no ficción. Colabora con medios como Etiqueta Negra, Paula, Virginia Quaterly Review, Internazionale, El País, Gatopardo, The New York Times, Univisón y otros. Ha participado en talleres de crónica con periodistas como Alberto Salcedo, Francisco, Goldman, Julio Villanueva Chang, Jon Lee Anderson y Alma Guillermoprieto. Fue Premio Nacional de Periodismo de Bolivia en 2008. Formó parte de las antologías de crónica Bolivia a toda costa, Hora boliviana, Un ilegal en el paraíso y The Football Crónicas. Tiene tres libros publicados: Los mercaderes del Che, La vida de las cosas y Rigor mortis. La Fundación para el Nuevo Periodismo Iberoamericano lo ha incluido en el movimiento Nuevos Cronistas de Indias, que reúne a algunas de las voces más talentosas del periodismo latinoamericano.",
+            'imagen'=>'images/speakers/AlexAyala.png'
+        ]);
 
-        // DB::table('speakers')->insert([
-        //     'nombre'=>"Alex Ayala",
-        //     'charla'=>"",
-        //     'descripcion'=>"Español de nacimiento, boliviano de corazón y tartamudo de vocación. Fue director del dominical del diario La Razón de Bolivia, editor de periodismo narrativo del semanario Pulso y fundador de Pie Izquierdo, primera revista boliviana de no ficción. Colabora con medios como Etiqueta Negra, Paula, Virginia Quaterly Review, Internazionale, El País, Gatopardo, The New York Times, Univisón y otros. Ha participado en talleres de crónica con periodistas como Alberto Salcedo, Francisco, Goldman, Julio Villanueva Chang, Jon Lee Anderson y Alma Guillermoprieto. Fue Premio Nacional de Periodismo de Bolivia en 2008. Formó parte de las antologías de crónica Bolivia a toda costa, Hora boliviana, Un ilegal en el paraíso y The Football Crónicas. Tiene tres libros publicados: Los mercaderes del Che, La vida de las cosas y Rigor mortis. La Fundación para el Nuevo Periodismo Iberoamericano lo ha incluido en el movimiento Nuevos Cronistas de Indias, que reúne a algunas de las voces más talentosas del periodismo latinoamericano.",
-        //     'imagen'=>'images/speakers/AlexAyala.png'
-        // ]);
-
-        for ($i=0; $i < 8; $i++) {
+        for ($i=0; $i < 6; $i++) {
             if($i % 2 === 0){
                 $im = "images/speakers/man.png";
             }
